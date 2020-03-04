e78ce21b4dd891cf7c671c4c41177d21e7de451b
Thomas <thomasvdoever@xs4all.nl>
2020-03-04 10:43:03 +0100

    added blinky functionallity to project testja

--- testja/src/main.cpp
+++ testja/src/main.cpp
@@ -1,10 +1,13 @@
 #include <mbed.h>
 
+DigitalOut myled(LED1);
+
+
 int main() {
-
-  // put your setup code here, to run once:
-
   while(1) {
-    // put your main code here, to run repeatedly:
+        myled = 1;
+        wait(1);
+        myled = 0;
+        wait(1);
   }
 }
