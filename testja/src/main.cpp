cc8f3a81aa2bab4eacbf7b79e742a1f53c680350
Thomas <thomasvdoever@xs4all.nl>
2020-03-04 10:56:10 +0100

    Inital commit of Foilboard project

--- testja/src/main.cpp
+++ testja/src/main.cpp
@@ -1,13 +1,16 @@
 #include <mbed.h>
 
 DigitalOut myled(LED1);
+DigitalOut myled2(LED2);
 
 
 int main() {
   while(1) {
         myled = 1;
+        myled2 = 0;
         wait(1);
         myled = 0;
+        myled2 = 1;
         wait(1);
   }
 }
