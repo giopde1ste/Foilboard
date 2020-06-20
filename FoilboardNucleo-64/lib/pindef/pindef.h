4742a9b4bae4b27e564d8f6a2067d26e13aefc53
Thomas <thomasvdoever@xs4all.nl>
2020-06-20 14:15:44 +0200

    fixed pindef.h not working

--- FoilboardNucleo-64/lib/pindef/pindef.h
+++ FoilboardNucleo-64/lib/pindef/pindef.h
@@ -2,8 +2,8 @@
 #define PINDEF_H
 
 #define PWM_RECIEVER PB_2
-#define LED1 PB2
-#define LED2 PB1
-#define LED3 PB15
+#define LED1 PB_2
+#define LED2 PB_1
+#define LED3 PB_15
 
 #endif
--- FoilboardNucleo-64/src/main.cpp
+++ FoilboardNucleo-64/src/main.cpp
@@ -1,6 +1,6 @@
 #include <mbed.h>
 #include <PwmIn.h>
-#incldue "pindef.h"
+#include "pindef.h"
 
 InterruptIn button(BUTTON1);
 DigitalOut led(LED1);
